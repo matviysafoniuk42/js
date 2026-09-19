8525191948a04f0e6f90de90d2fadbd26cfbe68c
Matviy <matvejsafonuk30@gmail.com>
2026-09-19 17:58:23 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const userForm = document.getElementById("userForm");
+
+userForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById("userName").value;
+  const email = document.getElementById("userEmail").value;
+
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  localStorage.setItem("userData", JSON.stringify(userData));
+
+  console.log("Дані збережено:", userData);
+
+  userForm.reset();
+
+  const modalElement = document.getElementById("userModal");
+  const modal = bootstrap.Modal.getInstance(modalElement);
+  modal.hide();
+});
